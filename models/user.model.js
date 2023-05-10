d228bcad7c0db1368c9223856f0ac531e0976418
AngelPs <65132700+AngelPena1@users.noreply.github.com>
2023-05-10 19:10:39 -0400

    Change on user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,17 +1,15 @@
 const db = require('../database/db.js')
 const {DataTypes} = require('sequelize')
-const PersonModel = require('./person.model.js')
+// const PersonModel = require('./person.model.js')
 
-const UserModel = db.define('users', {
-    person_Id: {type: DataTypes.INTEGER},
-    username: {type: DataTypes.STRING},
-    password: {type: DataTypes.TEXT},
-    active: {type: DataTypes.BOOLEAN},
-    refreshToken: {type: DataTypes.TEXT}
-})
-
-UserModel.belongsTo(PersonModel, {
-    foreignKey: 'person_Id'    
-});
+const UserModel = db.define('USUARIOS', {
+    ID_NEGOCIO: {type: DataTypes.INTEGER},
+    USUARIO: {type: DataTypes.STRING},
+    CLAVE: {type: DataTypes.STRING},
+    ACTIVO: {type: DataTypes.STRING}
+}, {
+    timestamps: false,
+    tableName: 'USUARIOS' // Nombre personalizado de la tabla
+  })
 
 module.exports = UserModel
