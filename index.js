dd09ac46370b22a76e4a8adb6ef9592e2164b14f
AngelPs <65132700+AngelPena1@users.noreply.github.com>
2023-05-18 17:55:45 -0400

    #

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ const app = express();
 app.use(logger)
 
 // app.use(credentials)
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: 'GET, POST, PUT, DELETE',
+  allowedHeaders: 'Content-Type, Authorization'
+}));
 // app.use(cors(corsOptions));
 
 app.use(
