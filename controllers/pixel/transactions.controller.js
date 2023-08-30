d76974bc64c3976f34fc0f409ab589d43095727a
Angelpena1 <Angelpenasantos2017@gmail.com>
2023-08-30 16:00:24 -0400

    adding controller transaction places

--- controllers/pixel/transactions.controller.js
+++ controllers/pixel/transactions.controller.js
@@ -1,6 +1,7 @@
 const TransactionModel = require("../../models/pixel/transactions.model.js");
 const DiscountModel = require("../../models/pixel/discount.model.js");
 const { Op } = require('sequelize'); 
+const sequelize = require('sequelize');
 require("dotenv").config();
 
 const getAllTransactions = async (req, res) => {
@@ -19,6 +20,41 @@ const getAllTransactions = async (req, res) => {
           attributes: ["monto"],
         },
       ],
+    });
+    res.json(transactions);
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
+
+const getTransactionPlaces = async (req, res) => {
+  try {
+    const { business_id, branch_id, first_date, second_date} = req.params;
+
+    const firstDate = () => {
+      const date = new Date(first_date)
+      const month = date.getMonth() + 1 > 9 ? `${date.getMonth() + 1}` : `0${date.getMonth() + 1}`
+      const day = date.getDate() > 9 ? `${date.getDate()}` : `0${date.getDate()}`
+      return `${date.getFullYear()}-${month}-${day}T00:00:00.000Z`
+    }
+
+    const secondDate = () => {
+      const date = new Date(second_date)
+      const month = date.getMonth() + 1 > 9 ? `${date.getMonth() + 1}` : `0${date.getMonth() + 1}`
+      const day = date.getDate() > 9 ? `${date.getDate()}` : `0${date.getDate()}`
+      return `${date.getFullYear()}-${month}-${day}T00:00:00.000Z`
+    }
+
+    const transactions = await TransactionModel.findAll({
+      attributes: ["descript1", [sequelize.fn("SUM", sequelize.col("total")), "total"]],
+      where: {
+        ID_NEGOCIO: business_id,
+        ID_SUCURSAL: branch_id,
+        opendate: {
+          [Op.between]: [firstDate(), secondDate()]
+        },
+      },
+      group: ["descript1"]
     });
     res.json(transactions);
   } catch (error) {
@@ -75,4 +111,4 @@ const getTransactionByEmployee = async (req, res) => {
   }
 };
 
-module.exports = { getAllTransactions, getByTransactId, getTransactionByEmployee };
+module.exports = { getAllTransactions, getTransactionPlaces, getByTransactId, getTransactionByEmployee };
